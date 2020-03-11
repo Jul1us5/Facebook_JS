73e5f8341dbdce093f184767589122d030c43758
Julius <Julius>
2020-03-11 11:51:00 +0200

    Add HTML / CSS | Create Array with ( some ) data

---
+++ JS/data.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const feed = [
+    {
+    autorius: {
+        vardas: 'Julius',
+        pavarde: 'Pavardenis',
+        amzius: 26,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#'
+    },
+        laikas: 1583832433393,
+    pranesimas: {
+        tekstas: 'Sveiki, as esu Julius.'
+    }
+    },
+
+    {
+    autorius: {
+        vardas: 'Evelina',
+        pavarde: 'Pavardenis',
+        amzius: 20,
+        lytis: 'Moteris',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+    },
+        laikas: 1583832433393,
+    pranesimas: {
+        tekstas: 'Neturiu komentaru....'
+    }
+    }
+]
+
