e7aea45e5498f8562c5204e8af2f05f1bc33ff75
Julius <Julius>
2020-03-11 16:18:18 +0200

    Function and Loop

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,43 @@
-"use strict";
+"use strict"; 
+
+function getFeed(list) {
+    if ( !Array.isArray(list) ) {
+        console.error('ERROR | give me array!');
+        
+    }
+    let HTML = '';
+
+    for (let i = 0; i < list.length; i++) {
+
+       const dataList = list[i];
+       HTML += getData(dataList);
+    }
+    return document.querySelector('.content').innerHTML = HTML;
+}
+
+function getData(list) {
+let HTML = `<div class="post">
+            ${getDataHeader( list.autorius, list.laikas )}
+            ${getDataContent( list.pranesimas )}
+            ${getDataFooter(  )}
+            </div>`;
+
+    return HTML;
+}
+
+function getDataHeader(autorius, laikas) {
+    let HTML = `<div class="header">Header :)</div>`;
+    return HTML;
+}
+function getDataContent(pranesimas) {
+    let HTML = `<div class="content">Content</div>`;
+    return HTML;
+}
+function getDataFooter( ) {
+    let HTML = `<div class="footer">Footer</div>`;
+    return HTML;
+}
 
 
-console.log(feed);
+
+getFeed(feed);
