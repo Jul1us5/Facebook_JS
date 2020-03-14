f3ac7c0101f0d8daa7cb48fa9a18c4a5f0c4add8
Julius <Julius>
2020-03-14 18:27:02 +0200

    updated

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,13 @@
 "use strict"; 
 
+
+
 function getFeed(list) {
     if ( !Array.isArray(list) ) {
         console.error('ERROR | give me array!');
         
     }
+    
     let HTML = '';
 
     for (let i = 0; i < list.length; i++) {
@@ -23,13 +26,25 @@ let HTML = `<div class="post">
             </div>`;
 
     return HTML;
+
 }
 
+
 function getDataHeader(autorius, laikas) {
+
+
+
     let HTML = `<div class="header">
+
                 <img src="./img/avatar/${autorius.avataras}">
+
+                <div class="user_title">
                 <div class="user_info">${autorius.vardas} ${autorius.pavarde}</div>
                 <div class="time">${laikas}</div>
+                </div>
+
+                <i class="fa fa-ellipsis-v"></i>
+
                 </div>`;
     return HTML;
 }
@@ -44,4 +59,20 @@ function getDataFooter( ) {
 
 
 
+        // for (var i = 0; i<feed.length; i++) {
+        //     var res = feed[i].autorius.avataras;
+        //     var avatr = '';
+        //     var all;
+        //     if ( res === 'avataras.png') {
+        //          all = avatr + 'user.png';
+        //     } else {
+        //          all = res;
+        //     }
+            
+        //     console.log(all);
+
+        // }
+
+
 getFeed(feed);
+
