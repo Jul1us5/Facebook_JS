f870fdb2e1fd94443e72d7cc3bf50724c5028dec
Julius <Julius>
2020-03-14 11:48:17 +0200

    updated data.js

--- JS/data.js
+++ JS/data.js
@@ -29,6 +29,168 @@ const feed = [
     pranesimas: {
         tekstas: 'Neturiu komentaru....'
     }
+    },
+
+    {
+    autorius: {
+        vardas: 'Tomas',
+        pavarde: 'Pavardenis',
+        amzius: 27,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi'
     }
+    },
+
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi . Ex earum tenetur odit nemo id architecto laborum iure error, . Ex earum tenetur odit nemo id architecto laborum iure error,'
+    }
+    },
+
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Labas'
+    }
+    },
+
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, veniam quia totam nemo, tempore delectus, illum rem necessitatibus minus maxime et odit vel explicabo ullam! Tenetur iusto velit delectus et ex numquam repellat natus, eaque esse sint iste consectetur placeat nesciunt quos vel commodi? Aliquam modi facilis nulla consequatur illum.'
+    }
+    },
+    
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Labas',
+        paveiksliukai: [
+            'image1.png',
+        ]
+    }
+    },
+
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Labas',
+        paveiksliukai: [
+            'image1.png',
+            'image2.png',
+        ]
+    }
+    }, 
+    
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+        pranesimas: {
+        tekstas: 'Labas',
+        paveiksliukai: [
+            'image1.png',
+            'image2.png',
+            'image3.png',
+        ]
+    }
+    }, 
+        
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+        },
+    laikas: 1583832434388,
+    pranesimas: {
+        tekstas: 'Labas',
+        paveiksliukai: [
+            'image1.png',
+            'image2.png',
+            'image3.png',
+            'image4.png',
+        ]
+    }
+    }, 
+            
+    {
+    autorius: {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis',
+        amzius: 50,
+        lytis: 'Vyras',
+        avataras: 'avataras.png',
+        nuoroda: '#',
+    },
+    laikas: 1583832434388,
+    pranesimas: {
+        tekstas: 'Labas',
+        paveiksliukai: [
+            'image1.png',
+            'image2.png',
+            'image3.png',
+            'image4.png',
+            'image5.png',
+            'image6.png',
+        ]
+    }
+    },    
+    
 ]
 
